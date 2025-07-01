be80c82934a76cfb554965173c6bbdf2c9b2b5a3
WalterLandaburo <walter.libreria@gmail.com>
2025-07-01 17:32:20 -0300

    Readme1
    Modificaciones:
    -------------------------
    • coloque la acción de ordenar la lista pincipal antes de guardar en el archivo de texto. Así se guarda ordenada la, información.
    • al momento de usar el bucle "for" para guardar, añadí la ubicación de cada valor de la sub-lista, lo que cambia la presentación en el archivo de texto.
    • dada la modificación al guardar, en el archivo, cada línea se lee: "name, lastname, email". Los valores aparecen separados por una coma (",") y no hay corchetes.
    • la nueva presentación genera una facilidad para leer el contenido. Incluso podría imprimirse o compartirse y ser fácil de leer para alguien que no tenga relación con la programación.

---
+++ Registro_Clientes_Ordenado_Legible.py
@@ -0,0 +1,106 @@
+"""
+
+registro_clientes_Salvar_Datos.py 
+
+Autor: Landaburo, Walter Nahuel (desarrollador autodidacta).
+Fecha de creación: 2025 - 07 - 01.
+
+Descripción:
+-----------
+Este script implementa una simulación básica de un registro de clientes.
+Permite agregar datos: nombre, apellido y correo electrónico a una lista principal y crear o sobrescribir un archivo de texto para guardar la información.
+El programa está diseñado para interactuar con el usuario/a.
+
+Estructura general:
+------------------
+• archivo de texto para guardar información.
+• docstring para documentar las acciones al recorrer el script.
+
+Modificaciones:
+------
+• coloque la acción de ordenar la lista pincipal antes de guardar en el archivo de texto. Así se guarda ordenada la, información.
+• al momento de usar el bucle "for" para guardar, añadí la ubicación de cada valor de la sub-lista, lo que cambia la presentación en el archivo de texto.
+• dada la modificación al guardar, en el archivo, cada línea se lee: "name, lastname, email". Los valores aparecen separados por una coma (",") y no hay corchetes.
+• la nueva presentación genera una facilidad para leer el contenido. Incluso podría imprimirse o compartirse y ser fácil de leer para alguien que no tenga relación con la programación.
+
+Licencia:
+--------
+Este código es de uso educativo y puede ser modificado con fines personales o de aprendizaje.
+
+Compatibilidad:
+---------------
+Python 3.0
+"""
+
+
+
+
+#lista principal 
+list_clients = []
+
+
+#Datos: nombre, apellido y correo electrónico 
+print("=== Registro de clientes,===")
+print("Ingrese los datos del cliente")
+
+ 
+
+while True:
+    
+    name = str(input("\nIngrese el nombre del cliente: ")).strip().title()
+        
+    #Salir del bucle 
+    if name == "":
+        print("Finalizar registro. Saliendo ....")
+        break
+            
+    lastname = str(input("\nIngrese el apellido del cliente: ")).strip().capitalize()
+
+    email = str(input("\nIngrese el correo electrónico del cliente: ")).strip()
+    
+    
+    #Sub-lista para cada cliente    
+    client = [name, lastname, email]
+    
+    #Añadir sub-lista a la lista principal 
+    list_clients.append(client)
+    
+    
+    """
+Archivo de texto:
+     guarda la información en un archivo de texto
+     usa 'w': significa que crea un archivo o sobrescribe uno existente con el nombre indicado 
+     cada vez que inicia el programa sobrescribe el archivo, lo que significa que desaparece la información guardada anteriormente, siendo reemplazada por la nueva
+     ubique la acción después de la acción de añadir a la lista para que sea inmediato y no dependa de la decisión del usuario cuando guardar. Así se disminuye el riesgo de la perdida de la información por una falla de energía que pueda cerrar la sesión abruptamente, no dando oportunidad al usuario/a de elegir guardar el registro
+     se guarda como una lista en cada línea, cada línea representa a un cliente, su apariencia es '[name, lastname, email]'
+     
+"""
+    archivo = open("datos.txt", "w")
+    
+    #Ordenar lista principal 
+    list_clients.sort()
+    
+    
+    #Guardar como "name, lastname, email"
+    for client in list_clients:
+        archivo.write(f"{client[0]}, {client[1]}, {client[2]}\n")
+        
+    archivo.close()
+
+
+    #Confirma la acción de registro del cliente 
+    print("\n\n✅ Cliente registrado")
+    print(f"Nombre: {client[0]} Apellido: {client[1]} Correo electrónico: {client[2]}")
+
+
+    
+    #Mostrar el contenido de la lista principal 
+    print("\n\nLista de clientes")
+    for i, client in enumerate(list_clients, start=1):
+        print(f"{i}. Nombre: {client[0]} Apellido: {client[1]} Correo electrónico: {client[2]}")
+    
+         
+        
+        
+  
+ 
